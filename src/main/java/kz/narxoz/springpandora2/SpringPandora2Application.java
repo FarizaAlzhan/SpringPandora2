b52a7e6a45c5ddc3f85f1fdc3a203245a866128b
FarizaAlzhan <fariza.alzhan@narxoz.kz>
2022-11-19 16:36:32 +0600

    first init (Fariza)

---
+++ src/main/java/kz/narxoz/springpandora2/SpringPandora2Application.java
@@ -0,0 +1,13 @@
+package kz.narxoz.springpandora2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPandora2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPandora2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kz/narxoz/springpandora2/SpringPandora2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.narxoz.springpandora2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPandora2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
